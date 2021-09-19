1da355e4d95e23c7309acda9e38cd6753584aad9
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-09-19 17:39:06 +0900

    스프링 MVC연습: Http 요청데이터 -API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        /*항상 바이트를 문자로 변환할때는 어떤 인코딩인지 알려줘야한다.
+        * */
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody); //postman에서 body에 넣었던 데이터 그대로 출력
+
+        response.getWriter().write("ok");
+    }
+}

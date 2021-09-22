726cd4bd914e0d7b02c40486f0f0337b0fee067d
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-09-22 17:37:20 +0900

    스프링 MVC연습: 어뎁터 컨트롤러2

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,7 +1,7 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 
 import javax.servlet.ServletException;
@@ -11,24 +11,30 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
-        //컨트롤러 v3로 구현한 무엇인가 넘어오게 되면 참을 반환
-        //먼저 프론트컨트롤러에서 서포트를 호출할때 걸러진게 애를 찾아서 handle 호출
-        return (handler instanceof ControllerV3);
+        //컨트롤러 v4를 지원하는지 체크
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        //(ControllerV4)로 캐스팅한다는 것은 타입을 ControllerV4 타입으로 바꾼다는 의미 이다.
+        ControllerV4 controller = (ControllerV4) handler;
 
-        ControllerV3 controller = (ControllerV3) handler;
+        //v4에서 이역활을 프론트컨트롤러에 구현했다면 이제는 어뎁터에다가 구현한다.
         Map<String, String> paramMap = createParamMap(request);
-        //반환 타입을 modelview로 맞춰서 반환해줘야 한다.
-        //실제 컨트롤러 호출
-        ModelView mv = controller.process(paramMap);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
         return mv;
     }
+
     private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
         //paramMap 으로 모든 파라미터값을 다 꺼내온다.
@@ -36,6 +42,4 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
                 .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
         return paramMap;
     }
-
-
 }
